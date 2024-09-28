b3b277ff1330644ef95777d3a75cc9307c4336f9
albertkingdom <albertkingdom@gmail.com>
2024-09-28 12:58:03 +0800

    add CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.handlers.order_handler import OrderHandler
 from app.models.order import Order
 from app.services.transformers import OrderTransformer
 from app.services.validators import OrderValidator
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/api/orders")
 async def create_order(order: Order):
